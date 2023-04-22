d1c913d2ae5971bfac364fb3c142e673ef0c38ee
Rodolfo187 <rodolfo.yy17@gmail.com>
2023-04-22 13:49:58 -0600

    Version pre-lanzamiento

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ const {connect} = require('mongoose')
 const conectarDB = async () =>{
     try{    
         await connect(process.env.MONGODB_URI);
-        console.log('Conectado exitosamente a MongoDB')
+        console.log('=============================================')
+        console.log('=============================================')
+        console.log('=====>>>>Conectado exitosamente a MongoDB <===========')
+        console.log('=============================================')
+        console.log('=============================================')
 
     }catch(error){
         console.log(error);
